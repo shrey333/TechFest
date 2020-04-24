f37f1063b49e62d8872539d92994754f62557ef3
h3ydra <h3ydra@github.com>
2020-04-24 07:32:48 +0530

    first commit

--- why/settings.py
+++ why/settings.py
@@ -89,8 +89,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'h3ydrahammer@gmail.com'
-EMAIL_HOST_PASSWORD = 'hammerhydra33'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your password'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
